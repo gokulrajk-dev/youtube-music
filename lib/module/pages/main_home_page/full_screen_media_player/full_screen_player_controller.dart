f45e5c70e3830f584967814cead21847db5bda16
gokulrajk-dev <kgokulraj.developer@gmail.com>
2026-03-08 22:20:18 +0530

    feat: add the local music feature

--- lib/module/pages/main_home_page/full_screen_media_player/full_screen_player_controller.dart
+++ lib/module/pages/main_home_page/full_screen_media_player/full_screen_player_controller.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
+import 'package:just_audio/just_audio.dart';
 
 class full_screen_media_player_controller extends GetxController{
+  final AudioPlayer player = AudioPlayer();
+
 
 
 }
