5aef4a861b817f9f45cd4ca4a81c36ba0ce955de
gokulrajk-dev <kgokulraj.developer@gmail.com>
2026-07-10 19:28:13 +0530

    fix

--- lib/module/pages/main_home_page/main_home_page_controller.dart
+++ lib/module/pages/main_home_page/main_home_page_controller.dart
@@ -6,6 +6,16 @@ import '../library/library_views.dart';
 class Main_Home_Page_Controller extends GetxController {
   var current_index = 0.obs;
 
+  Future<void> refreshHome(List<Future> futures)async{
+    try{
+      get_isloading(true);
+      Future.wait(futures);
+      update();
+    }finally{
+      get_isloading(false);
+    }
+  }
+
   void get_current_index(int index) {
     if (current_index.value == index && index == 3) {
       Get.bottomSheet(
